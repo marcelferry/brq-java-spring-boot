bfa112f335ddf1f630fe82c684534b06df036b07
Marcelo Ferreira <marcelferry@gmail.com>
2017-06-30 17:43:06 -0300

    Correct BookService

--- book-rest/src/main/java/com/brq/digital/workshop/bookrest/BookRepository.java
+++ book-rest/src/main/java/com/brq/digital/workshop/bookrest/BookRepository.java
@@ -3,7 +3,6 @@ package com.brq.digital.workshop.bookrest;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Transactional
 public interface BookRepository extends JpaRepository<Book, Long> {
